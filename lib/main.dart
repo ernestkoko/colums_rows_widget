899db37eb95c10f246138830e294a5804c4face8
ernestkoko <ernesteferetin@yahoo.com>
2020-04-30 08:24:46 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rows and Columns',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Rows and Columns'),
+        ),
+        backgroundColor: Colors.indigo[100],
+        body: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //takes the full width of the body
+    final sizeX = MediaQuery.of(context).size.width;
+    //it takes the full size of the height of the the widget
+    final sizeY = MediaQuery.of(context).size.height;
+    return Container(
+      width: sizeX,
+      height: sizeY,
+      child: Row(
+//        verticalDirection: VerticalDirection.up,
+//        textDirection: TextDirection.rtl,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: createSquares(5),
+      ),
+    );
+  }
+
+  List<Widget> createSquares(int numSquares) {
+    int i = 0;
+    List colors = [
+      Colors.amber,
+      Colors.deepPurple,
+      Colors.deepOrange,
+      Colors.indigo,
+      Colors.lightBlue
+    ];
+    List<Widget> squares = List<Widget>();
+    while (i < numSquares) {
+      Expanded square = Expanded(
+          flex: i,
+          child: Container(
+            color: colors[i],
+//        width: 60.0,
+          height: 60.0,
+            child: Text(i.toString()),
+          ));
+      //increment the counter
+      i++;
+      //add the square
+      squares.add(square);
+    }
+    return squares;
+  }
+}
